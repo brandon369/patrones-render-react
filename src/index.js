0d4972908d9c5e0704ed25c309ba605e117d7a96
Brandon A <byarrieta@protonmail.com>
2022-08-02 22:14:22 -0500

    hocs init

--- src/index.js
+++ src/index.js
@@ -3,7 +3,29 @@ import ReactDOM from 'react-dom';
 import App from './App/index.js';
 import './index.css';
 
+
+// function App(props) {
+//   return (
+//     <h1>Hi {props.name}!</h1>
+//   )
+// }
+//
+// function withWhatever(WrappedComponent) {
+//   return function ComponenteDeVerdad(props) {
+//     return (
+//       <React.Fragment>
+//         <WrappedComponent {...props}/>
+//         <h2>Tamos acompañando al wrapped</h2>
+//       </React.Fragment>
+//     )
+//   }
+// }
+//
+//
+// const AppWithWhatever = withWhatever(App)
+
 ReactDOM.render(
-  <App />,
+  <App name='Chamaco'/>,
+  // <AppWithWhatever name='Chamaco'/>,
   document.getElementById('root')
 );
